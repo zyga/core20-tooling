992e4f53ca0ce4db9558f7711bdde94cc71316ad
Zygmunt Krynicki <me@zygoon.pl>
2019-06-03 12:38:13 +0200

    cmd/snapd-boot: print kernel command line
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- cmd/snapd-boot/main.go
+++ cmd/snapd-boot/main.go
@@ -1,9 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/google/shlex"
+)
+
+// kernelArgs returns the kernel command line, as read from /proc/cmdline
+func kernelArgs() ([]string, error) {
+	bytes, err := ioutil.ReadFile("/proc/cmdline")
+	if err != nil {
+		return nil, err
+	}
+	return shlex.Split(string(bytes))
+}
 
 func main() {
 	fmt.Println("snapd-boot")
+	args, err := kernelArgs()
+	if err != nil {
+		fmt.Printf("cannot read kernel command line: %s", err)
+		return
+	}
+	fmt.Printf("kernel command line: %#v\n", args)
 	// TODO:
 	// - determine system boot mode, either RUN or RECOVERY.
 	// - determine current BOOT-SET (should be "factory" in this demo)

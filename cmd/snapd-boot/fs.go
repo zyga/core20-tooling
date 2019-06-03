877d62b49c2e68c14d63f46dcff5a0afdff9f992
Zygmunt Krynicki <me@zygoon.pl>
2019-06-03 14:42:58 +0200

    cmd: perform all fs setup from go
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- cmd/snapd-boot/fs.go
+++ cmd/snapd-boot/fs.go
@@ -18,6 +18,18 @@ func createSkeletonFileSystem() error {
 	if err := mountDevPts(); err != nil {
 		return err
 	}
+	if err := mountRun(); err != nil {
+		return err
+	}
+	paths := []string{"/bin", "/etc", "/tmp", "/var", "/var/lock", "/run/initramfs"}
+	for _, path := range paths {
+		if err := mkdir(path, 0755); err != nil {
+			return err
+		}
+	}
+	if err := syscall.Symlink("/proc/mounts", "/etc/mtab"); err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -55,10 +67,25 @@ func mountDevPts() error {
 	return mountFundamental(source, target, fstype, flags, "gid=5,mode=0620")
 }
 
+func mountRun() error {
+	const source = "tmpfs"
+	const target = "/run"
+	const fstype = "tmpfs"
+	const flags = syscall.MS_NOEXEC | syscall.MS_NOSUID
+	return mountFundamental(source, target, fstype, flags, "size=10%,mode=0755")
+}
+
 func mountFundamental(source, target, fstype string, flags uintptr, data string) error {
-	// The kernel creates /dev/console so some directories may already exist.
-	if err := os.Mkdir(target, 0755); err != nil && !os.IsExist(err) {
+	if err := mkdir(target, 0755); err != nil {
 		return err
 	}
 	return syscall.Mount(source, target, fstype, flags, data)
+}
+
+// The kernel creates /dev/console so some directories may already exist.
+func mkdir(path string, mode os.FileMode) error {
+	if err := os.Mkdir(path, mode); err != nil && !os.IsExist(err) {
+		return err
+	}
+	return nil
 }
--- cmd/snapd-init/snapd-init.c
+++ cmd/snapd-init/snapd-init.c
@@ -10,53 +10,6 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-static int do_mkdir(const char *path, mode_t mode) {
-    if (mkdir(path, mode) < 0) {
-        switch (errno) {
-            case EEXIST:
-                fprintf(stderr, "kernel already created: %s\n", path);
-                break;
-            default:
-                fprintf(stderr, "cannot create directory %s: %m\n", path);
-                return -1;
-        }
-    }
-    return 0;
-}
-
-static int do_mkdir_many(const char **dnames, mode_t mode) {
-    for (const char **dname = dnames; *dname != NULL; ++dname) {
-        if (do_mkdir(*dname, mode) < 0) {
-            return -1;
-        }
-    }
-    return 0;
-}
-
-static int construct_skeleton_fs(void) {
-    const char *dnames[] = {
-		/* NOTE: kernel creates /dev and /root internally */
-		/* NOTE: /proc is mounted by snapd-boot */
-		/* NOTE: /sys is mounted by snapd-boot */
-        "/bin", "/etc", "/run", "/tmp", "/var", "/var/lock", NULL,
-    };
-    if (do_mkdir_many(dnames, 0755) < 0) {
-        return -1;
-    }
-    if (mount("tmpfs", "/run", "tmpfs", MS_NOEXEC | MS_NOSUID, "size=10%,mode=0755") < 0) {
-        fprintf(stderr, "cannot mount /run: %m\n");
-        return -1;
-    }
-    if (do_mkdir("/run/initramfs", 0755) < 0) {
-        return -1;
-    }
-    if (symlink("/proc/mounts", "/etc/mtab") < 0) {
-        fprintf(stderr, "cannot symlink /proc/mounts as /etc/mtab: %m\n");
-        return -1;
-    }
-    return 0;
-}
-
 static int construct_busybox_farm(void) {
     if (access("/busybox-static", X_OK) != 0) {
         return -1;
@@ -132,9 +85,6 @@ int main(int argc, char **argv) {
     setlinebuf(stdout);
     printf("snapd-init\n");
     if (call_snapd_boot() < 0) {
-        return 1;
-    }
-    if (construct_skeleton_fs() < 0) {
         return 1;
     }
     if (construct_busybox_farm() == 0) {
